97c313685e52fc3e391cab69f55636c6056c7dd1
Santhosh Ragavu <ragavudev@gmail.com>
2024-08-10 21:27:45 +0530

    Added WhyAscender componenet

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Banner from "./components/Banner";
+import WhyAscender from "./components/WhyAscender";
 
 function App() {
   return (
     <>
       <Banner />
+      <WhyAscender />
     </>
   );
 }
